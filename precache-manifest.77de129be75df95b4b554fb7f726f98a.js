0e8ebedc4aeaf3b752f733f5af587508b6df1e15
Lance Turri <lance.turri@gmail.com>
2019-03-24 20:55:56 -0500

    deploy

---
+++ precache-manifest.77de129be75df95b4b554fb7f726f98a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d89be0ebfcb2760c06c9",
+    "url": "/js/about.1383eeaa.js"
+  },
+  {
+    "revision": "78b1e1805740b67dd9d2",
+    "url": "/css/app.5662aa11.css"
+  },
+  {
+    "revision": "78b1e1805740b67dd9d2",
+    "url": "/js/app.c566a69a.js"
+  },
+  {
+    "revision": "90464016482834d88d44",
+    "url": "/css/chunk-vendors.a4198fbb.css"
+  },
+  {
+    "revision": "90464016482834d88d44",
+    "url": "/js/chunk-vendors.27f01a8c.js"
+  },
+  {
+    "revision": "960c0384bac4bcf8e5b2b390b558f7d3",
+    "url": "/img/constellation.960c0384.png"
+  },
+  {
+    "revision": "f04c29da88193d4959cffe0084199c00",
+    "url": "/img/bulbs.f04c29da.png"
+  },
+  {
+    "revision": "17b9bc130ac1b3b978d48535506a6b02",
+    "url": "/img/mars.17b9bc13.png"
+  },
+  {
+    "revision": "2f75e8725324bcb66a24203a59a32706",
+    "url": "/img/underwater.2f75e872.png"
+  },
+  {
+    "revision": "c5108e56f485c806c1d32f74960605fe",
+    "url": "/img/mountains.c5108e56.png"
+  },
+  {
+    "revision": "ddb771a1456b45935cb7ac32a1483001",
+    "url": "/img/unicorn.ddb771a1.png"
+  },
+  {
+    "revision": "8143b5d2fbcff99adc5cdd7f0fe669f5",
+    "url": "/img/wild.8143b5d2.png"
+  },
+  {
+    "revision": "f1316b36363bcfa6304282b328b41c53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
